4b03234003da68fe249c0c1ef563f13a290b3c51
sudhir kumar singh <sudhirsingh@m-ize.com>
2012-10-01 12:40:59 +0530

    Adding social media into Brand support

--- src/main/java/com/mize/domain/brand/BrandSupportFeedback.java
+++ src/main/java/com/mize/domain/brand/BrandSupportFeedback.java
@@ -1,5 +1,7 @@
 package com.mize.domain.brand;
 
+import java.util.List;
+
 import org.codehaus.jackson.map.annotate.JsonDeserialize;
 import org.codehaus.jackson.map.annotate.JsonSerialize;
 import org.joda.time.DateTime;
@@ -13,35 +15,35 @@ public class BrandSupportFeedback extends Entity {
 
 	private int supportLogId;
 	private int id;
+	private Brand brand;
+	private int userId;
+	private String userName;
+	private String supportType;
 	private int rating;
 	private String feedback;
 	private boolean resolved;	
 	private DateTime feedbackDate;
 	private String ticketNo;
-	private Brand brand;
-	private int userId;
-	private int userName;
-	private String supportType;
-	
+	private List<PostToSocialMedia> socialMediaPosts;
+
 	public BrandSupportFeedback(){
 		
 	}
 
-	public BrandSupportFeedback(int supportLogId, int id, int rating,
-			String feedback, boolean resolved, DateTime feedbackDate,
-			String ticketNo, Brand brand, int userId, int userName,
-			String supportType) {
+	public BrandSupportFeedback(int supportLogId, int id, Brand brand, int userId, String userName, String supportType, int rating,
+			String feedback, boolean resolved, DateTime feedbackDate, String ticketNo, List<PostToSocialMedia> socialMediaPosts) {
 		this.supportLogId = supportLogId;
 		this.id = id;
+		this.brand = brand;
+		this.userId = userId;
+		this.userName = userName;
+		this.supportType = supportType;
 		this.rating = rating;
 		this.feedback = feedback;
 		this.resolved = resolved;
 		this.feedbackDate = feedbackDate;
 		this.ticketNo = ticketNo;
-		this.brand = brand;
-		this.userId = userId;
-		this.userName = userName;
-		this.supportType = supportType;
+		this.socialMediaPosts = socialMediaPosts;
 	}
 
 	public int getSupportLogId() {
@@ -58,6 +60,38 @@ public class BrandSupportFeedback extends Entity {
 
 	public void setId(int id) {
 		this.id = id;
+	}
+
+	public Brand getBrand() {
+		return brand;
+	}
+
+	public void setBrand(Brand brand) {
+		this.brand = brand;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getSupportType() {
+		return supportType;
+	}
+
+	public void setSupportType(String supportType) {
+		this.supportType = supportType;
 	}
 
 	public int getRating() {
@@ -104,35 +138,27 @@ public class BrandSupportFeedback extends Entity {
 		this.ticketNo = ticketNo;
 	}
 
-	public Brand getBrand() {
-		return brand;
+	public List<PostToSocialMedia> getSocialMediaPosts() {
+		return socialMediaPosts;
 	}
 
-	public void setBrand(Brand brand) {
-		this.brand = brand;
+	public void setSocialMediaPosts(List<PostToSocialMedia> socialMediaPosts) {
+		this.socialMediaPosts = socialMediaPosts;
 	}
 
-	public int getUserId() {
-		return userId;
-	}
-
-	public void setUserId(int userId) {
-		this.userId = userId;
-	}
-
-	public int getUserName() {
-		return userName;
-	}
-
-	public void setUserName(int userName) {
-		this.userName = userName;
-	}
-
-	public String getSupportType() {
-		return supportType;
-	}
-
-	public void setSupportType(String supportType) {
-		this.supportType = supportType;
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("supportLogId = " + supportLogId + ",");
+		sb.append("brand = " + brand + ",");
+		sb.append("userId = " + userId + ",");
+		sb.append("userName = " + userName + ",");
+		sb.append("supportType = " + supportType + ",");
+		sb.append("rating = " + rating + ",");
+		sb.append("feedback = " + feedback + ",");
+		sb.append("resolved = " + resolved + ",");
+		sb.append("feedbackDate = " + feedbackDate + ",");
+		sb.append("ticketNo = " + ticketNo + ",");
+		sb.append("socialMediaPosts = " + socialMediaPosts + "} \n");		
+		return sb.toString();
 	}
 }
