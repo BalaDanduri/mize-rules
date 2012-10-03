1040b33f8e3a512fd0cad75b837499f870481b2a
sudhir kumar singh <sudhirsingh@m-ize.com>
2012-10-03 17:42:37 +0530

    Adding date format

--- src/main/java/com/mize/domain/util/JodaDateTimeDeserializer.java
+++ src/main/java/com/mize/domain/util/JodaDateTimeDeserializer.java
@@ -13,6 +13,7 @@ import org.joda.time.format.DateTimeFormatter;
 public class JodaDateTimeDeserializer extends JsonDeserializer<DateTime> {
 
 	private static final DateTimeFormatter  dateFormat = DateTimeFormat.forPattern("MM-dd-yyyy HH:mm:ss");
+	public static final DateTimeFormatter  dbdateFormat = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
 
 	@Override
 	public DateTime deserialize(JsonParser parser, DeserializationContext context)
