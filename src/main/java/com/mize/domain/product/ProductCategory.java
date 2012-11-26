ddab9221bd9d684fe86cc42f2aa7c834ffdef3d5
Suresh Vemulapalli <SureshVemulapalli@m-ize.com>
2012-11-26 18:16:00 +0530

    Update to domains

--- src/main/java/com/mize/domain/product/ProductCategory.java
+++ src/main/java/com/mize/domain/product/ProductCategory.java
@@ -24,25 +24,16 @@ public class ProductCategory extends Entity{
 	protected ProductCategory parent;
 	@JsonIgnore
 	protected Set<ProductCategory> children;
-	protected boolean isLeaf;
 	protected ProdCategorySource sourceCategory;
+	protected boolean isActive;
 	
 	public ProductCategory() {
-		// TODO Auto-generated constructor stub
 		parent = new ProductCategory();
 		children = new HashSet<ProductCategory>();
 		sourceCategory = new ProdCategorySource();
 	}
 
 	
-//	public ProductCategory(Long id, String name, String link, ProductCategory parent) {
-//		this.id = id;
-//		this.name = name;
-//		this.photoLink = link;
-//		this.parent = parent;
-//	}
-//
-
 	public Long getId() {
 		return id;
 	}
@@ -76,14 +67,36 @@ public class ProductCategory extends Entity{
 	}
 
 
+	public boolean isActive() {
+		return isActive;
+	}
+
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+
+
+	@Override
+	public String toString() {
+		return "ProductCategory [id=" + id + ", name=" + name + ", link=" + photoLink
+				+ ", parent=" + parent + "]";
+	}
+
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result
+				+ ((children == null) ? 0 : children.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((photoLink == null) ? 0 : photoLink.hashCode());
+		result = prime * result + (isActive ? 1231 : 1237);
 		result = prime * result + ((name == null) ? 0 : name.hashCode());
 		result = prime * result + ((parent == null) ? 0 : parent.hashCode());
+		result = prime * result
+				+ ((photoLink == null) ? 0 : photoLink.hashCode());
+		result = prime * result
+				+ ((sourceCategory == null) ? 0 : sourceCategory.hashCode());
 		return result;
 	}
 
@@ -97,15 +110,17 @@ public class ProductCategory extends Entity{
 		if (getClass() != obj.getClass())
 			return false;
 		ProductCategory other = (ProductCategory) obj;
+		if (children == null) {
+			if (other.children != null)
+				return false;
+		} else if (!children.equals(other.children))
+			return false;
 		if (id == null) {
 			if (other.id != null)
 				return false;
 		} else if (!id.equals(other.id))
 			return false;
-		if (photoLink == null) {
-			if (other.photoLink != null)
-				return false;
-		} else if (!photoLink.equals(other.photoLink))
+		if (isActive != other.isActive)
 			return false;
 		if (name == null) {
 			if (other.name != null)
@@ -117,17 +132,20 @@ public class ProductCategory extends Entity{
 				return false;
 		} else if (!parent.equals(other.parent))
 			return false;
+		if (photoLink == null) {
+			if (other.photoLink != null)
+				return false;
+		} else if (!photoLink.equals(other.photoLink))
+			return false;
+		if (sourceCategory == null) {
+			if (other.sourceCategory != null)
+				return false;
+		} else if (!sourceCategory.equals(other.sourceCategory))
+			return false;
 		return true;
 	}
 
 
-	@Override
-	public String toString() {
-		return "Category [id=" + id + ", name=" + name + ", link=" + photoLink
-				+ ", parent=" + parent + "]";
-	}
-
-	
 	public static void main(String args[]) throws JsonGenerationException, JsonMappingException, IOException {
 		
 //		ProductCategory cat1 = new ProductCategory(new Long(1), "Electronics", null, null );
@@ -149,14 +167,17 @@ public class ProductCategory extends Entity{
 		this.children = children;
 	}
 
-
 	public boolean isLeaf() {
-		return isLeaf;
+		if ((children == null || children.isEmpty()) ) {
+//		if ((children == null || children.isEmpty()) && (parent!= null)) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
-
-	public void setLeaf(boolean isLeaf) {
-		this.isLeaf = isLeaf;
+	public void setLeaf(boolean leaf) {
+		// Not needed as it is based on the Children
 	}
 
 
