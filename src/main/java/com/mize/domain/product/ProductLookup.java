baba7900060cb1de6c9a1c8ee3950639786bc8d7
Suresh Vemulapalli <SureshVemulapalli@m-ize.com>
2012-12-05 16:51:20 +0530

    Adding a new domain object for Product lookup.

---
+++ src/main/java/com/mize/domain/product/ProductLookup.java
@@ -0,0 +1,94 @@
+package com.mize.domain.product;
+
+public class ProductLookup {
+		
+	public static final String SOURCE_MIZE = "1";
+	public static final String SOURCE_AMAZON = "2";
+	public static final String SOURCE_GOOGLE = "3";
+	
+	public static final String LOOKUP_ASIN = "ASIN";
+	public static final String LOOKUP_UPC = "UPC";
+	public static final String LOOKUP_SKU = "SKU";
+	public static final String LOOKUP_ISBN = "ISBN";
+	public static final String LOOKUP_EAN = "EAN";
+	
+	protected Long sourceId;
+	protected String lookupKey;
+	protected String lookupId;
+
+	
+	public ProductLookup() {
+	}
+	
+	public String getLookupKey() {
+		return lookupKey;
+	}
+	
+	public void setLookupKey(String LookupKey) {
+		this.lookupKey = LookupKey;
+	}
+
+	public Long getSourceId() {
+		return sourceId;
+	}
+	
+	public void setSourceId(Long sourceId) {
+		this.sourceId = sourceId;
+	}
+
+	public String getLookupId() {
+		return lookupId;
+	}
+	
+	public void setLookupId(String LookupId) {
+		this.lookupId = LookupId;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((lookupId == null) ? 0 : lookupId.hashCode());
+		result = prime * result
+				+ ((lookupKey == null) ? 0 : lookupKey.hashCode());
+		result = prime * result
+				+ ((sourceId == null) ? 0 : sourceId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ProductLookup other = (ProductLookup) obj;
+		if (lookupId == null) {
+			if (other.lookupId != null)
+				return false;
+		} else if (!lookupId.equals(other.lookupId))
+			return false;
+		if (lookupKey == null) {
+			if (other.lookupKey != null)
+				return false;
+		} else if (!lookupKey.equals(other.lookupKey))
+			return false;
+		if (sourceId == null) {
+			if (other.sourceId != null)
+				return false;
+		} else if (!sourceId.equals(other.sourceId))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "ProductLookup [sourceId=" + sourceId + ", lookupKey="
+				+ lookupKey + ", lookupId=" + lookupId + "]";
+	}
+
+
+}
