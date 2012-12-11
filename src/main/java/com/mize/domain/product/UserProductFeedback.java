932c613736d152075e41cb33d90a3a4dc8f46ede
Suresh Vemulapalli <SureshVemulapalli@m-ize.com>
2012-12-11 12:13:04 +0530

    Added feedback title to

--- src/main/java/com/mize/domain/product/UserProductFeedback.java
+++ src/main/java/com/mize/domain/product/UserProductFeedback.java
@@ -14,6 +14,7 @@ import com.mize.domain.util.JsonDateTimeSerializer;
 public class UserProductFeedback  extends Entity{
 	
 	protected Long id;
+	protected String feedbackTitle;
 	protected String review;
 	protected Integer rating;
 	protected User user;
@@ -25,19 +26,24 @@ public class UserProductFeedback  extends Entity{
 	
 
 	@Override
+	public String toString() {
+		return "UserProductFeedback [id=" + id + ", feedbackTitle="
+				+ feedbackTitle + ", review=" + review + ", rating=" + rating
+				+ ", user=" + user + ", product=" + product + "]";
+	}
+
+
+	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result
-				+ ((id == null) ? 0 : id.hashCode());
+				+ ((feedbackTitle == null) ? 0 : feedbackTitle.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((product == null) ? 0 : product.hashCode());
+		result = prime * result + ((rating == null) ? 0 : rating.hashCode());
 		result = prime * result + ((review == null) ? 0 : review.hashCode());
-		result = prime * result
-				+ ((rating == null) ? 0 : rating.hashCode());
 		result = prime * result + ((user == null) ? 0 : user.hashCode());
-		result = prime * result
-				+ ((product == null) ? 0 : product.hashCode());
-		result = prime * result + ((review == null) ? 0 : review.hashCode());
-
 		return result;
 	}
 
@@ -51,39 +57,37 @@ public class UserProductFeedback  extends Entity{
 		if (getClass() != obj.getClass())
 			return false;
 		UserProductFeedback other = (UserProductFeedback) obj;
+		if (feedbackTitle == null) {
+			if (other.feedbackTitle != null)
+				return false;
+		} else if (!feedbackTitle.equals(other.feedbackTitle))
+			return false;
 		if (id == null) {
-			if (other.getId() != null)
+			if (other.id != null)
 				return false;
-		} else if (!id.equals(other.getId()))
-			return false;
-		if (review == null) {
-			if (other.getReview() != null)
-				return false;
-		} else if (!review.equals(other.getReview()))
-			return false;
-		if (rating == null) {
-			if (other.getRating()!= null)
-				return false;
-		} else if (!rating.equals(other.getRating()))
-			return false;
-		if (user == null) {
-			if (other.getUser() != null)
-				return false;
-		} else if (!user.equals(other.user))
+		} else if (!id.equals(other.id))
 			return false;
 		if (product == null) {
 			if (other.product != null)
 				return false;
 		} else if (!product.equals(other.product))
 			return false;
+		if (rating == null) {
+			if (other.rating != null)
+				return false;
+		} else if (!rating.equals(other.rating))
+			return false;
+		if (review == null) {
+			if (other.review != null)
+				return false;
+		} else if (!review.equals(other.review))
+			return false;
+		if (user == null) {
+			if (other.user != null)
+				return false;
+		} else if (!user.equals(other.user))
+			return false;
 		return true;
-	}
-
-
-	@Override
-	public String toString() {
-		return "ProductReview [id=" + id + ", review=" + review + ", rating="
-				+ rating + ", userId=" + user + ", product=" + product + "]";
 	}
 
 
@@ -94,6 +98,16 @@ public class UserProductFeedback  extends Entity{
 
 	public void setId(Long id) {
 		this.id = id;
+	}
+
+	
+	public String getFeedbackTitle() {
+		return feedbackTitle;
+	}
+
+
+	public void setFeedbackTitle(String feedbackTitle) {
+		this.feedbackTitle = feedbackTitle;
 	}
 
 
