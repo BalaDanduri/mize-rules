88766cf814e00551569c34677c3d8d2cb3de3038
sudhir kumar singh <sudhirsingh@m-ize.com>
2013-02-27 22:19:55 +0530

    adding price and buy objects

---
+++ src/main/java/com/mize/domain/product/ProductPrices.java
@@ -0,0 +1,38 @@
+package com.mize.domain.product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductPrices {
+	
+	private List<ProductPrice> productPrices = new ArrayList<ProductPrice>();
+	
+	public class ProductPrice {
+		private Double price;
+		private String sourceName;
+		public Double getPrice() {
+			return price;
+		}
+		public void setPrice(Double price) {
+			this.price = price;
+		}
+		public String getSourceName() {
+			return sourceName;
+		}
+		public void setSourceName(String sourceName) {
+			this.sourceName = sourceName;
+		}
+	}
+
+	public List<ProductPrice> getProductPrices() {
+		return productPrices;
+	}
+
+	public void setProductPrices(List<ProductPrice> productPrices) {
+		this.productPrices = productPrices;
+	}
+	
+	public void addProductPrice(ProductPrice productPrice) {
+		this.productPrices.add(productPrice);
+	}
+}
