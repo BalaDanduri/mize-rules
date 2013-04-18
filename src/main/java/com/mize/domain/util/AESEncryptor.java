2ccbc59bca0c8851023b57413d37c3ea0a9e7ce4
Rama <ramabattu@m-ize.com>
2013-04-17 20:42:13 -0400

    test jenkins auto compilation.

--- src/main/java/com/mize/domain/util/AESEncryptor.java
+++ src/main/java/com/mize/domain/util/AESEncryptor.java
@@ -8,8 +8,7 @@ import sun.misc.BASE64Encoder;
 
 @SuppressWarnings("restriction")
 public class AESEncryptor {
-	
-    //shared secret
+    //shared secret test
     static byte[] keyBytes = new byte[] { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
                                         0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
     static String sPadding = "ISO10126Padding";
