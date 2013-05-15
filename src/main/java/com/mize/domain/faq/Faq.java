02a5ab6ac0d0fb1fe24a71cf911a786187c3227a
baladanduri <baladanduri@MIZE-LAP-009.mizeidc.mize.local>
2013-05-15 11:55:57 +0530

    Added domains to FAQ

---
+++ src/main/java/com/mize/domain/faq/Faq.java
@@ -0,0 +1,44 @@
+package com.mize.domain.faq;
+
+import com.mize.domain.common.Entity;
+
+public class Faq extends Entity{
+	private static final long serialVersionUID = 5713977396952999898L;
+	private String brandName;
+	private String productName;
+	private Long seqNo;
+	private String question;
+	private String answer;
+	
+	public String getBrandName() {
+		return brandName;
+	}
+	public void setBrandName(String brandName) {
+		this.brandName = brandName;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public Long getSeqNo() {
+		return seqNo;
+	}
+	public void setSeqNo(Long seqNo) {
+		this.seqNo = seqNo;
+	}
+	public String getQuestion() {
+		return question;
+	}
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+	public String getAnswer() {
+		return answer;
+	}
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+
+}
