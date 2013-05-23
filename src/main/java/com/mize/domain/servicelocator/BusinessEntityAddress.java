baf7b7146bf598dac0958a76f916a0557543a921
baladanduri <baladanduri@MIZE-LAP-009.mizeidc.mize.local>
2013-05-23 17:35:40 +0530

    domain changes

--- src/main/java/com/mize/domain/servicelocator/BusinessEntityAddress.java
+++ src/main/java/com/mize/domain/servicelocator/BusinessEntityAddress.java
@@ -6,8 +6,8 @@ import org.codehaus.jackson.annotate.JsonIgnore;
 
 import com.mize.domain.common.Country;
 import com.mize.domain.common.Entity;
+import com.mize.domain.common.Locale;
 import com.mize.domain.common.State;
-import com.mize.domain.util.Locale;
 
 public class BusinessEntityAddress  extends Entity  implements Comparable<BusinessEntityAddress>{
 
