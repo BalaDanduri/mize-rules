fe9d98260acba08a561f74d9f84ee7a0b18a2efe
Rama <ramabattu@m-ize.com>
2013-06-10 15:17:58 -0400

    fixed compilation issue.

--- src/main/java/com/mize/domain/socialnetwork/SocialConstants.java
+++ src/main/java/com/mize/domain/socialnetwork/SocialConstants.java
@@ -8,6 +8,7 @@ public class SocialConstants {
     public static final String TWIT_FORBIDDEN = "403:";
     
     public static final String TWIT_NO_CONNECTIONS="";
+    public static final String ERROR_NO_CONNECTIONS ="";
     
     
     public static final String ERROR_INVALID_TOKEN = "ACCESS_TOKEN_INVALID";
