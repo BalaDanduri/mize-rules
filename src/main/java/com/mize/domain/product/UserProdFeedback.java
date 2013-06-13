3592400f19a7ec5d0f09f39b66e3d5384b19eadd
baladanduri <baladanduri@MIZE-LAP-009.mizeidc.mize.local>
2013-06-13 17:03:52 +0530

    update domain

--- src/main/java/com/mize/domain/product/UserProdFeedback.java
+++ src/main/java/com/mize/domain/product/UserProdFeedback.java
@@ -4,14 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import com.mize.domain.common.Entity;
 import com.mize.domain.user.FriendsActivities;
+import com.mize.domain.user.UserFriend;
 
-public class UserProdFeedback {
+public class UserProdFeedback extends Entity{
+	
+	private static final long serialVersionUID = -3952329917212635913L;
 	Map<Long, FriendsActivities> commentsMap ;
 	Map<Long, FriendsActivities> likeMap;
 	Map<Long, FriendsActivities> userLikedMap ;
 	List<UserProductFeedback>  prodFeedbacks;
-	
+	private List<UserFriend> userFriends;
 	
 	public Map<Long, FriendsActivities> getCommentsMap() {
 		return commentsMap;
@@ -45,5 +49,21 @@ public class UserProdFeedback {
 			}
 		}
 		return feedbackIds;
+	}
+	public List<UserFriend> getUserFriends() {
+		return userFriends;
+	}
+	public void setUserFriends(List<UserFriend> userFriends) {
+		this.userFriends = userFriends;
+	}
+	
+	public List<Long> getUserIds() {
+		List<Long> userIds = new ArrayList<Long>();
+		if (userFriends != null) {
+			for (UserFriend user2 : userFriends) {
+				userIds.add(user2.getFriendUser().getId());
+			}
+		}
+		return userIds;
 	}
 }
