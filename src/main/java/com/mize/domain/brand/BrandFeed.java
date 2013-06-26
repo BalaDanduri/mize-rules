34f4795fd231477e4ce4e3e8056f514edd693c78
baladanduri <baladanduri@MIZE-LAP-009.mizeidc.mize.local>
2013-06-26 11:13:54 +0530

    added new domain

---
+++ src/main/java/com/mize/domain/brand/BrandFeed.java
@@ -0,0 +1,72 @@
+package com.mize.domain.brand;
+
+import org.codehaus.jackson.map.annotate.JsonDeserialize;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+import org.joda.time.DateTime;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.mize.domain.common.Entity;
+import com.mize.domain.util.JodaDateTimeDeserializer;
+import com.mize.domain.util.JsonDateTimeSerializer;
+
+public class BrandFeed extends Entity{
+	
+	private static final long serialVersionUID = -8006941720446513027L;
+	private Long id;
+	private Brand brand;
+	private String feedType;
+	private String feedDesc;
+	@DateTimeFormat (pattern="MM-dd-yyyy h:mm:ss")
+	private DateTime startTime;
+	@DateTimeFormat (pattern="MM-dd-yyyy h:mm:ss")
+	private DateTime endTime;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Brand getBrand() {
+		return brand;
+	}
+	public void setBrand(Brand brand) {
+		this.brand = brand;
+	}
+	public String getFeedType() {
+		return feedType;
+	}
+	public void setFeedType(String feedType) {
+		this.feedType = feedType;
+	}
+	public String getFeedDesc() {
+		return feedDesc;
+	}
+	public void setFeedDesc(String feedDesc) {
+		this.feedDesc = feedDesc;
+	}
+	
+	@DateTimeFormat (pattern="MM-dd-yyyy h:mm:ss")
+	@JsonSerialize(using=JsonDateTimeSerializer.class)
+	public DateTime getStartTime() {
+		return startTime;
+	}
+	
+	@DateTimeFormat (pattern="MM-dd-yyyy h:mm:ss")
+	@JsonDeserialize(using=JodaDateTimeDeserializer.class)	
+	public void setStartTime(DateTime startTime) {
+		this.startTime = startTime;
+	}
+	
+	@DateTimeFormat (pattern="MM-dd-yyyy h:mm:ss")
+	@JsonSerialize(using=JsonDateTimeSerializer.class)
+	public DateTime getEndTime() {
+		return endTime;
+	}
+	
+	@DateTimeFormat (pattern="MM-dd-yyyy h:mm:ss")
+	@JsonDeserialize(using=JodaDateTimeDeserializer.class)	
+	public void setEndTime(DateTime endTime) {
+		this.endTime = endTime;
+	}
+}
