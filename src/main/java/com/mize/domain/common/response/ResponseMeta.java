31d3e7586de35a3182fd6bbdb5ce1494597ef69a
sureshvemulapallimize <SureshVemulapalli@m-ize.com>
2013-07-10 11:44:33 +0530

    Update to response meta data.

--- src/main/java/com/mize/domain/common/response/ResponseMeta.java
+++ src/main/java/com/mize/domain/common/response/ResponseMeta.java
@@ -7,105 +7,105 @@ import org.codehaus.jackson.annotate.JsonIgnore;
 
 public class ResponseMeta {
 	
-	private String statusCode;
-	private List<String> statusMessage;
-	private List<ResponseStatus> status;
+	private String status;
+	private List<ResponseStatus> errors;
 	private Integer resultSize;
 	private Long totalRecords;
 	private Integer pageNumber;
 	private Long totalPages;
-	private Long responseTime = System.currentTimeMillis();
 	private String version;
 	private String request;
 	@JsonIgnore
-	private Long startTime = System.currentTimeMillis();
+	private final Long startTime = System.currentTimeMillis();
 
+	public static final String STATUS_SUCCESS = "success";
+	public static final String STATUS_FAILURE = "failure";
+	
 	public ResponseMeta() {
 		
 	}
 	
-	public ResponseMeta(String statusCode, List<String> statusMessage, Integer resultSize, Long totalRecords,
-			Integer pageNumber, Long totalPages, Long responseTime, String version, String request) {
-		this.statusCode = statusCode;
-		this.statusMessage = statusMessage;
-		this.resultSize = resultSize;
-		this.totalRecords = totalRecords;
-		this.pageNumber = pageNumber;
-		this.totalPages = totalPages;
-		this.version = version;
-		this.request = request;
-	}
-	
-	public ResponseMeta(String statusCode, List<String> statusMessage, Integer resultSize, Long totalRecords,
-			Integer pageNumber, Long totalPages, Long responseTime, String version, String request, List<ResponseStatus> status) {
-		this.statusCode = statusCode;
-		this.statusMessage = statusMessage;
-		this.resultSize = resultSize;
-		this.totalRecords = totalRecords;
-		this.pageNumber = pageNumber;
-		this.totalPages = totalPages;
-		this.version = version;
-		this.request = request;
+	public ResponseMeta(String status, List<ResponseStatus> errors, Integer resultSize, Long totalRecords,
+			Integer pageNumber, Long totalPages, String version, String request) {
 		this.status = status;
-	}
-
-	public String getStatusCode() {
-		return statusCode;
-	}
-	public void setStatusCode(String statusCode) {
-		this.statusCode = statusCode;
-	}
-	public List<String> getStatusMessage() {
-		return statusMessage;
-	}
-	public void setStatusMessage(List<String> statusMessage) {
-		this.statusMessage = statusMessage;
-	}
-	public Integer getResultSize() {
-		return resultSize;
-	}
-	public void setResultSize(Integer resultSize) {
+		this.errors = errors;
 		this.resultSize = resultSize;
-	}
-	public Long getTotalRecords() {
-		return totalRecords;
-	}
-	public void setTotalRecords(Long totalRecords) {
 		this.totalRecords = totalRecords;
-	}
-	public Integer getPageNumber() {
-		return pageNumber;
-	}
-	public void setPageNumber(Integer pageNumber) {
 		this.pageNumber = pageNumber;
-	}
-	public Long getTotalPages() {
-		return totalPages;
-	}
-	public void setTotalPages(Long totalPages) {
 		this.totalPages = totalPages;
-	}
-	public Long getResponseTime() {
-		return (System.currentTimeMillis()-responseTime);
-	}
-	public String getVersion() {
-		return version;
-	}
-	public void setVersion(String version) {
 		this.version = version;
-	}
-	public String getRequest() {
-		return request;
-	}
-	public void setRequest(String request) {
 		this.request = request;
 	}
 
-	public List<ResponseStatus> getStatus() {
+	public String getStatus() {
+		if (status == null || !status.equalsIgnoreCase(STATUS_FAILURE)) {
+			status = STATUS_SUCCESS;
+		}
 		return status;
 	}
 
-	public void setStatus(List<ResponseStatus> status) {
+	public void setStatus(String status) {
 		this.status = status;
-	}	
+	}
+
+	public List<ResponseStatus> getErrors() {
+		return errors;
+	}
+
+	public void setErrors(List<ResponseStatus> errors) {
+		this.errors = errors;
+	}
+
+	public Integer getResultSize() {
+		return resultSize;
+	}
+
+	public void setResultSize(Integer resultSize) {
+		this.resultSize = resultSize;
+	}
+
+	public Long getTotalRecords() {
+		return totalRecords;
+	}
+
+	public void setTotalRecords(Long totalRecords) {
+		this.totalRecords = totalRecords;
+	}
+
+	public Integer getPageNumber() {
+		return pageNumber;
+	}
+
+	public void setPageNumber(Integer pageNumber) {
+		this.pageNumber = pageNumber;
+	}
+
+	public Long getTotalPages() {
+		return totalPages;
+	}
+
+	public void setTotalPages(Long totalPages) {
+		this.totalPages = totalPages;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+	public String getRequest() {
+		return request;
+	}
+
+	public void setRequest(String request) {
+		this.request = request;
+	}
+	
+	public Long getResponseTime() {
+		return System.currentTimeMillis() - startTime;
+	}
+
 }
