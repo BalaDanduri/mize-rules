e558a91755d6294f144ff6df1597dcea851feeac
sureshvemulapallimize <SureshVemulapalli@m-ize.com>
2013-07-23 16:16:14 +0530

    Adding error constant

---
+++ src/main/java/com/mize/domain/exception/ErrorConstants.java
@@ -0,0 +1,7 @@
+package com.mize.domain.exception;
+
+public class ErrorConstants {
+	public static final String SQL_ERROR = "SQL_ERROR";
+	public static final String TECHNICAL_ERROR = "GEN_TECH_001";
+
+}
