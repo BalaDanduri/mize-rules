986e30234ea46007bd1ab1c51990d998aeb3c96c
baladanduri <baladanduri@MIZE-LAP-009.mizeidc.mize.local>
2013-07-24 12:49:12 +0530

    added new fileds

--- src/main/java/com/mize/domain/product/ProductRegister.java
+++ src/main/java/com/mize/domain/product/ProductRegister.java
@@ -1,7 +1,10 @@
 package com.mize.domain.product;
 
+import java.util.List;
+
 import org.codehaus.jackson.map.annotate.JsonDeserialize;
 import org.codehaus.jackson.map.annotate.JsonSerialize;
+import org.codehaus.jackson.map.annotate.JsonSerialize.Inclusion;
 import org.joda.time.DateTime;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -32,12 +35,12 @@ public class ProductRegister extends MizeEntity{
 	private String address3;
 	private String city;
 	private String state;
-	private String zipCode;
 	private String country;
+	private String zipCode;
 	private String phoneMobile;
 	private String phoneHome;
 	private String phoneWork;
-	
+	private List<ProductRegnAttachment> attachments ;
 	
 	@Override
 	public Long getId() {
@@ -69,7 +72,7 @@ public class ProductRegister extends MizeEntity{
 	}
 	
 	@DateTimeFormat (pattern="MM-dd-yyyy")
-	@JsonSerialize(using=JsonDateSerializer.class)
+	@JsonSerialize(using=JsonDateSerializer.class,include=Inclusion.NON_DEFAULT)
 	public DateTime getPurchaseDate() {
 		return purchaseDate;
 	}
@@ -92,7 +95,7 @@ public class ProductRegister extends MizeEntity{
 		this.purchaseStore = purchaseStore;
 	}
 	@DateTimeFormat (pattern="MM-dd-yyyy")
-	@JsonSerialize(using=JsonDateSerializer.class)
+	@JsonSerialize(using=JsonDateSerializer.class,include=Inclusion.NON_DEFAULT)
 	public DateTime getWarrantyExpiryDate() {
 		return warrantyExpiryDate;
 	}
@@ -185,7 +188,12 @@ public class ProductRegister extends MizeEntity{
 	public void setPhoneWork(String phoneWork) {
 		this.phoneWork = phoneWork;
 	}
-
+	public List<ProductRegnAttachment> getAttachments() {
+		return attachments;
+	}
+	public void setAttachments(List<ProductRegnAttachment> attachments) {
+		this.attachments = attachments;
+	}
 	@Override
 	public String toString() {
 		return "ProductRegister [product=" + product + ", user=" + user + ", serialNumber=" + serialNumber
@@ -193,18 +201,37 @@ public class ProductRegister extends MizeEntity{
 				+ purchaseStore + ", warrantyExpiryDate=" + warrantyExpiryDate + ", additionalInfo=" + additionalInfo
 				+ ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + ", address1=" + address1
 				+ ", address2=" + address2 + ", address3=" + address3 + ", city=" + city + ", state=" + state
-				+ ", zipCode=" + zipCode + ", country=" + country + ", phoneMobile=" + phoneMobile + ", phoneHome="
-				+ phoneHome + ", phoneWork=" + phoneWork + "]";
+				+ ", country=" + country + ", zipCode=" + zipCode + ", phoneMobile=" + phoneMobile + ", phoneHome="
+				+ phoneHome + ", phoneWork=" + phoneWork + ", attachments=" + attachments + "]";
 	}
-	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = super.hashCode();
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((additionalInfo == null) ? 0 : additionalInfo.hashCode());
+		result = prime * result + ((address1 == null) ? 0 : address1.hashCode());
+		result = prime * result + ((address2 == null) ? 0 : address2.hashCode());
+		result = prime * result + ((address3 == null) ? 0 : address3.hashCode());
+		result = prime * result + ((attachments == null) ? 0 : attachments.hashCode());
+		result = prime * result + ((city == null) ? 0 : city.hashCode());
+		result = prime * result + ((country == null) ? 0 : country.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result + ((phoneHome == null) ? 0 : phoneHome.hashCode());
+		result = prime * result + ((phoneMobile == null) ? 0 : phoneMobile.hashCode());
+		result = prime * result + ((phoneWork == null) ? 0 : phoneWork.hashCode());
+		result = prime * result + ((product == null) ? 0 : product.hashCode());
+		result = prime * result + ((purchaseDate == null) ? 0 : purchaseDate.hashCode());
+		result = prime * result + ((purchasePrice == null) ? 0 : purchasePrice.hashCode());
+		result = prime * result + ((purchaseStore == null) ? 0 : purchaseStore.hashCode());
+		result = prime * result + ((serialNumber == null) ? 0 : serialNumber.hashCode());
+		result = prime * result + ((state == null) ? 0 : state.hashCode());
+		result = prime * result + ((user == null) ? 0 : user.hashCode());
+		result = prime * result + ((warrantyExpiryDate == null) ? 0 : warrantyExpiryDate.hashCode());
+		result = prime * result + ((zipCode == null) ? 0 : zipCode.hashCode());
 		return result;
 	}
-
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -213,11 +240,116 @@ public class ProductRegister extends MizeEntity{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		ProductRegnAttachment other = (ProductRegnAttachment) obj;
-		if (id == null) {
-			if (other.getId() != null)
+		ProductRegister other = (ProductRegister) obj;
+		if (additionalInfo == null) {
+			if (other.additionalInfo != null)
 				return false;
-		} else if (!id.equals(other.getId()))
+		} else if (!additionalInfo.equals(other.additionalInfo))
+			return false;
+		if (address1 == null) {
+			if (other.address1 != null)
+				return false;
+		} else if (!address1.equals(other.address1))
+			return false;
+		if (address2 == null) {
+			if (other.address2 != null)
+				return false;
+		} else if (!address2.equals(other.address2))
+			return false;
+		if (address3 == null) {
+			if (other.address3 != null)
+				return false;
+		} else if (!address3.equals(other.address3))
+			return false;
+		if (attachments == null) {
+			if (other.attachments != null)
+				return false;
+		} else if (!attachments.equals(other.attachments))
+			return false;
+		if (city == null) {
+			if (other.city != null)
+				return false;
+		} else if (!city.equals(other.city))
+			return false;
+		if (country == null) {
+			if (other.country != null)
+				return false;
+		} else if (!country.equals(other.country))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		if (phoneHome == null) {
+			if (other.phoneHome != null)
+				return false;
+		} else if (!phoneHome.equals(other.phoneHome))
+			return false;
+		if (phoneMobile == null) {
+			if (other.phoneMobile != null)
+				return false;
+		} else if (!phoneMobile.equals(other.phoneMobile))
+			return false;
+		if (phoneWork == null) {
+			if (other.phoneWork != null)
+				return false;
+		} else if (!phoneWork.equals(other.phoneWork))
+			return false;
+		if (product == null) {
+			if (other.product != null)
+				return false;
+		} else if (!product.equals(other.product))
+			return false;
+		if (purchaseDate == null) {
+			if (other.purchaseDate != null)
+				return false;
+		} else if (!purchaseDate.equals(other.purchaseDate))
+			return false;
+		if (purchasePrice == null) {
+			if (other.purchasePrice != null)
+				return false;
+		} else if (!purchasePrice.equals(other.purchasePrice))
+			return false;
+		if (purchaseStore == null) {
+			if (other.purchaseStore != null)
+				return false;
+		} else if (!purchaseStore.equals(other.purchaseStore))
+			return false;
+		if (serialNumber == null) {
+			if (other.serialNumber != null)
+				return false;
+		} else if (!serialNumber.equals(other.serialNumber))
+			return false;
+		if (state == null) {
+			if (other.state != null)
+				return false;
+		} else if (!state.equals(other.state))
+			return false;
+		if (user == null) {
+			if (other.user != null)
+				return false;
+		} else if (!user.equals(other.user))
+			return false;
+		if (warrantyExpiryDate == null) {
+			if (other.warrantyExpiryDate != null)
+				return false;
+		} else if (!warrantyExpiryDate.equals(other.warrantyExpiryDate))
+			return false;
+		if (zipCode == null) {
+			if (other.zipCode != null)
+				return false;
+		} else if (!zipCode.equals(other.zipCode))
 			return false;
 		return true;
 	}
