a285738a7cfeb2016b022a7296bdc2f61767a938
sureshvemulapallimize <SureshVemulapalli@m-ize.com>
2013-08-05 16:12:17 +0530

    Adding new domain object for searching value additions

---
+++ src/main/java/com/mize/domain/product/MizeValueAdditionCriteria.java
@@ -0,0 +1,42 @@
+package com.mize.domain.product;
+
+import com.mize.domain.common.MizeEntity;
+
+public class MizeValueAdditionCriteria extends MizeEntity implements Comparable<MizeValueAdditionCriteria>{
+	private static final long serialVersionUID = 155385665556860537L;
+	private String productId;
+	private String upc;
+
+	@Override
+	public Long getId(){
+		return id;
+	}
+	
+	@Override
+	public void setId(Long id){
+		this.id = id;
+	}	
+	
+
+	public String getProductId() {
+		return productId;
+	}
+
+	public void setProductId(String productId) {
+		this.productId = productId;
+	}
+
+	public String getUpc() {
+		return upc;
+	}
+
+	public void setUpc(String upc) {
+		this.upc = upc;
+	}
+
+	@Override
+	public int compareTo(MizeValueAdditionCriteria arg0) {
+		return 0;
+	}
+	
+}
