d26366d53597d6bcce326e083d98d260976af29e
baladanduri@m-ize.com <Bala1010>
2013-08-14 15:27:52 +0530

    updated domains

--- src/main/java/com/mize/domain/util/MessageConstants.java
+++ src/main/java/com/mize/domain/util/MessageConstants.java
@@ -11,7 +11,12 @@ public abstract class MessageConstants implements Serializable{
 	public static String INVALID_ADDRESS = "INVALID_ADDRESS";
 	public static String CREATED_BY_REQUIRED = "CREATED_BY_REQUIRED";
 	public static String INVALID_ENTITY_TYPE_CODE = "VALID_ENTITY_TYPE_CODE_REQUIRED";
-	public static String PARENT_RECORD_NOT_FOUND = "PARENT_RECORD_NOT_FOUND";
+	public static String PRODUCT_ID_NOT_FOUND = "PRODUCT_ID_NOT_FOUND";
+	public static String SIMILAR_PRODUCT_ID_NOT_FOUND = "SIMILAR_PRODUCT_ID_NOT_FOUND";
+	public static String ACCESSORY_PRODUCT_ID_NOT_FOUND = "ACCESSORY_PRODUCT_ID_NOT_FOUND";
+	public static String UPSELL_PRODUCT_ID_NOT_FOUND = "UPSELL_PRODUCT_ID_NOT_FOUND";
+	public static String ATTRIBUTE_ID_NOT_FOUND = "ATTRIBUTE_ID_NOT_FOUND";
+	public static String CATEGORY_ID_NOT_FOUND = "CATEGORY_ID_NOT_FOUND";
 	public static String EXCEPTION = "EXCEPTION";
 
 }
