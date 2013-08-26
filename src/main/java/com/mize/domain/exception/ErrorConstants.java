7b49ad4c618e68d10802dc2a296379232458c4fa
baladanduri <baladanduri@MIZE-LAP-009.mizeidc.mize.local>
2013-08-26 11:36:58 +0530

    updated message constants

--- src/main/java/com/mize/domain/exception/ErrorConstants.java
+++
@@ -1,7 +0,0 @@
-package com.mize.domain.exception;
-
-public class ErrorConstants {
-	public static final String SQL_ERROR = "SQL_ERROR";
-	public static final String TECHNICAL_ERROR = "GEN_TECH_001";
-
-}
