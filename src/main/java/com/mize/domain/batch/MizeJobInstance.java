06a2e2dbb3df54254e10a94dc6a6d5b1d72e2ff9
sureshvemulapallimize <SureshVemulapalli@m-ize.com>
2013-09-05 11:21:07 +0530

    MizeJobInstance changes

--- src/main/java/com/mize/domain/batch/MizeJobInstance.java
+++ src/main/java/com/mize/domain/batch/MizeJobInstance.java
@@ -10,28 +10,28 @@ public class MizeJobInstance extends MizeEntity{
 	 */
 	private static final long serialVersionUID = -1638699269504919603L;
 	private Long id;
-	private Long jobId;
+	private MizeJob job;
 	private String instanceCode;
 	private String instanceName;
 	private Date lastRun;
 	private Date nextRun;
-	public String getJobName() {
-		return instanceCode;
-	}
-	public void setJobName(String jobCode) {
-		this.instanceCode = jobCode;
-	}
-	public Long getJobId() {
-		return jobId;
-	}
-	public void setJobId(Long jobId) {
-		this.jobId = jobId;
-	}
 	public Long getId() {
 		return id;
 	}
 	public void setId(Long id) {
 		this.id = id;
+	}
+	public MizeJob getJob() {
+		return job;
+	}
+	public void setJob(MizeJob job) {
+		this.job = job;
+	}
+	public String getInstanceCode() {
+		return instanceCode;
+	}
+	public void setInstanceCode(String instanceCode) {
+		this.instanceCode = instanceCode;
 	}
 	public String getInstanceName() {
 		return instanceName;
@@ -51,16 +51,41 @@ public class MizeJobInstance extends MizeEntity{
 	public void setNextRun(Date nextRun) {
 		this.nextRun = nextRun;
 	}
-	public String getInstanceCode() {
-		return instanceCode;
-	}
-	public void setInstanceCode(String instanceCode) {
-		this.instanceCode = instanceCode;
-	}
 	@Override
 	public String toString() {
-		return "MizeJobInstance [id=" + id + ", jobId=" + jobId + ", instanceCode=" + instanceCode + ", instanceName="
-				+ instanceName + ", lastRun=" + lastRun + ", nextRun=" + nextRun + "]";
+		return "MizeJobInstance [id=" + id + ", job=" + job + ", instanceCode="
+				+ instanceCode + ", instanceName=" + instanceName
+				+ ", lastRun=" + lastRun + ", nextRun=" + nextRun + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result
+				+ ((instanceCode == null) ? 0 : instanceCode.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MizeJobInstance other = (MizeJobInstance) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (instanceCode == null) {
+			if (other.instanceCode != null)
+				return false;
+		} else if (!instanceCode.equals(other.instanceCode))
+			return false;
+		return true;
 	}
 	
 }
