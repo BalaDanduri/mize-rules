9b36a46cabd05f0eba40e354847a98ee5f826c6e
baladanduri <baladanduri@m-ize.com>
2013-09-11 11:08:16 +0530

    updated domains

--- src/main/java/com/mize/domain/util/MessageConstants.java
+++ src/main/java/com/mize/domain/util/MessageConstants.java
@@ -46,4 +46,5 @@ public abstract class MessageConstants implements Serializable{
 	public static final String PRODUCT_ID_OR_BRAND_ID_REQUIRED = "PRODUCT_ID_OR_BRAND_ID_REQUIRED";
 	public static final String JDBC_UPDATE_ZERO_RECORDS = "ZERO_RECORDS_UPDATE";
 	public static final String DAO_ERROR = "DAO_ERROR";
+	public static final String INVALID_COUNTRY_OR_STATE = "INVALID_COUNTRY_OR_STATE";
 }
