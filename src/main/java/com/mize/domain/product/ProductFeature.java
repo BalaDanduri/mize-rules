978282ea21933f6957cda0d26ea2b30c6b5de864
baladanduri <baladanduri@m-ize.com>
2013-09-24 14:40:04 +0530

    updated domains

--- src/main/java/com/mize/domain/product/ProductFeature.java
+++ src/main/java/com/mize/domain/product/ProductFeature.java
@@ -1,13 +1,17 @@
 package com.mize.domain.product;
 
-import com.mize.domain.common.Entity;
+import java.util.List;
 
-public class ProductFeature extends Entity{
+import com.mize.domain.common.MizeEntity;
+
+public class ProductFeature extends MizeEntity implements Comparable<ProductFeature>{
 	
 	private static final long serialVersionUID = -97276364683192909L;
 	private String feature;
 	private String name;
 	private String absoluteValue;
+	private String groupName;
+	private List<ProductFeature> attributes;
 
 	public ProductFeature(String feature) {
 		super();
@@ -51,5 +55,37 @@ public class ProductFeature extends Entity{
 
 	public void setName(String name) {
 		this.name = name;
-	}	
+	}
+
+	public String getGroupName() {
+		return groupName;
+	}
+
+	public void setGroupName(String groupName) {
+		this.groupName = groupName;
+	}
+
+	public List<ProductFeature> getAttributes() {
+		return attributes;
+	}
+
+	public void setAttributes(List<ProductFeature> attributes) {
+		this.attributes = attributes;
+	}
+
+	@Override
+	public int compareTo(ProductFeature arg0) {
+		return 0;
+	}
+
+	@Override
+	public Long getId() {
+		return id;
+	}
+
+	@Override
+	public void setId(Long id) {
+		this.id = id;
+	}		
+	
 }
