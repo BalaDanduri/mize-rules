080d341c58715baaeb1bcba7627dafb6419a6502
harishburra <harishburra@m-ize.com>
2013-10-14 13:18:00 +0530

    Unit test cases for CatalogEntry and CatalogEntryIntl domains

--- src/main/java/com/mize/domain/sce/catalog/CatalogEntryIntl.java
+++ src/main/java/com/mize/domain/sce/catalog/CatalogEntryIntl.java
@@ -9,10 +9,16 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import org.codehaus.jackson.annotate.JsonIgnore;
+import org.codehaus.jackson.map.annotate.JsonDeserialize;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+import org.joda.time.DateTime;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import com.mize.domain.common.Locale;
 import com.mize.domain.common.MizeEntity;
+import com.mize.domain.util.JodaDateTimeDeserializer;
 
 @Entity
 @Table(name = "catalog_entry_intl")
@@ -88,6 +94,64 @@ public class CatalogEntryIntl extends MizeEntity {
 
 	public void setLocale(Locale locale) {
 		this.locale = locale;
+	}
+	
+	@Override	
+	@DateTimeFormat(pattern = "MM-dd-yyyy HH:mm:ss")
+	@Type(type="com.mize.domain.util.DateTimeJPA")
+	@Column(name = "created_date")
+	public DateTime getCreatedDate() {
+		return createdDate;
+	}
+	
+	@Override
+	@DateTimeFormat (pattern = "MM-dd-yyyy HH:mm:ss")
+	@JsonDeserialize(using=JodaDateTimeDeserializer.class)	
+	@JsonIgnore
+	public void setCreatedDate(DateTime createdDate) {
+		super.createdDate = createdDate;
+	}
+	
+	@Override	
+	@DateTimeFormat(pattern = "MM-dd-yyyy HH:mm:ss")
+	@Type(type="com.mize.domain.util.DateTimeJPA")
+	@Column(name = "updated_date")
+	public DateTime getUpdatedDate() {
+		return updatedDate;
+	}
+	
+	@Override
+	@DateTimeFormat (pattern = "MM-dd-yyyy HH:mm:ss")
+	@JsonDeserialize(using=JodaDateTimeDeserializer.class)	
+	@JsonIgnore
+	public void setUpdatedDate(DateTime updatedDate) {
+		super.updatedDate = updatedDate;
+	}
+	
+	@Override
+	@JsonIgnore
+	@Column(name = "created_by")
+	public Long getCreatedBy() {		
+		return super.getCreatedBy();
+	}
+	
+	@Override
+	@JsonIgnore
+	public void setCreatedBy(Long createdBy) {		
+		super.setCreatedBy(createdBy);
+	}
+	
+	@Override
+	@JsonIgnore
+	@Column(name = "updated_by")
+	public Long getUpdatedBy() {		
+		return super.getUpdatedBy();
+	}
+	
+	@Override
+	@JsonIgnore
+	public void setUpdatedBy(Long updatedBy) {		
+		super.setUpdatedBy(updatedBy);
 	}
 
 	@Override
