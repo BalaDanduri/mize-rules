b0291307c91b4795a16d90c0bb41c44acc7e1a6d
harishburra <harishburra@m-ize.com>
2013-11-07 15:34:11 +0530

    Inspection messages

--- src/main/java/com/mize/domain/util/MessageConstants.java
+++ src/main/java/com/mize/domain/util/MessageConstants.java
@@ -66,5 +66,6 @@ public abstract class MessageConstants implements Serializable{
 	public static final String INVALID_MODEL_SERIAL_COMBINATION = "INVALID_MODEL_AND_SERIAL_COMBINATION";
 	public static final String FORM_ALREADY_EXISTS = "FORM_ALREADY_EXISTS";
 	public static final String BUSINESS_ENTITY_CODE_REQUIRED = "BUSINESS_ENTITY_CODE_REQUIRED";
+	public static final String INVALID_BUSINESS_ENTITY_CODE = "INVALID_BUSINESS_ENTITY_CODE";
 	
 }
