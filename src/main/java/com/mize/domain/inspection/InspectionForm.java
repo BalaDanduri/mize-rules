a8bc359b9e02efb1763a0424cc31b0d363c64ed6
baladanduri@m-ize.com <Bala1010>
2013-11-08 16:20:34 +0530

    updated domains

--- src/main/java/com/mize/domain/inspection/InspectionForm.java
+++ src/main/java/com/mize/domain/inspection/InspectionForm.java
@@ -31,7 +31,7 @@ import com.mize.domain.util.JsonDateSerializer;
 
 @Entity
 @Table( name = "inspection_form")
-public class InspectionForm extends MizeEntity {
+public class InspectionForm extends MizeEntity implements Comparable<InspectionForm>{
 	
 	private static final long serialVersionUID = -666112603626187346L;
 	
@@ -239,111 +239,9 @@ public class InspectionForm extends MizeEntity {
 	}
 
 	@Override
-	public int hashCode() {
-		final int prime = PRIME;
-		int result = super.hashCode();
-		result = prime * result
-				+ ((businessEntity == null) ? 0 : businessEntity.hashCode());
-		result = prime
-				* result
-				+ ((businessEntityContact == null) ? 0 : businessEntityContact
-						.hashCode());
-		result = prime * result
-				+ ((formAttachments == null) ? 0 : formAttachments.hashCode());
-		result = prime * result
-				+ ((formAudits == null) ? 0 : formAudits.hashCode());
-		result = prime * result
-				+ ((formDefinition == null) ? 0 : formDefinition.hashCode());
-		result = prime * result
-				+ ((inspectedBy == null) ? 0 : inspectedBy.hashCode());
-		result = prime * result
-				+ ((inspectionCode == null) ? 0 : inspectionCode.hashCode());
-		result = prime * result
-				+ ((inspectionDate == null) ? 0 : inspectionDate.hashCode());
-		result = prime
-				* result
-				+ ((productRegistration == null) ? 0 : productRegistration
-						.hashCode());
-		result = prime * result
-				+ ((statusCode == null) ? 0 : statusCode.hashCode());
-		result = prime * result
-				+ ((template == null) ? 0 : template.hashCode());
-		result = prime * result
-				+ ((typeCode == null) ? 0 : typeCode.hashCode());
-		return result;
+	public int compareTo(InspectionForm arg0) {
+		return 0;
 	}
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (!super.equals(obj))
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		InspectionForm other = (InspectionForm) obj;
-		if (businessEntity == null) {
-			if (other.businessEntity != null)
-				return false;
-		} else if (!businessEntity.getId().equals(other.businessEntity.getId()))
-			return false;
-		if (businessEntityContact == null) {
-			if (other.businessEntityContact != null)
-				return false;
-		} else if (!businessEntityContact.equals(other.businessEntityContact))
-			return false;
-		if (formAttachments == null) {
-			if (other.formAttachments != null)
-				return false;
-		} else if (!formAttachments.equals(other.formAttachments))
-			return false;
-		if (formAudits == null) {
-			if (other.formAudits != null)
-				return false;
-		} else if (!formAudits.equals(other.formAudits))
-			return false;
-		if (formDefinition == null) {
-			if (other.formDefinition != null)
-				return false;
-		} else if (!formDefinition.equals(other.formDefinition))
-			return false;
-		if (inspectedBy == null) {
-			if (other.inspectedBy != null)
-				return false;
-		} else if (!inspectedBy.equals(other.inspectedBy))
-			return false;
-		if (inspectionCode == null) {
-			if (other.inspectionCode != null)
-				return false;
-		} else if (!inspectionCode.equals(other.inspectionCode))
-			return false;
-		if (inspectionDate == null) {
-			if (other.inspectionDate != null)
-				return false;
-		} else if (!inspectionDate.equals(other.inspectionDate))
-			return false;
-		if (productRegistration == null) {
-			if (other.productRegistration != null)
-				return false;
-		} else if (!productRegistration.getId().equals(other.productRegistration.getId()))
-			return false;
-		if (statusCode == null) {
-			if (other.statusCode != null)
-				return false;
-		} else if (!statusCode.equals(other.statusCode))
-			return false;
-		if (template == null) {
-			if (other.template != null)
-				return false;
-		} else if (!template.getId().equals(other.template.getId()))
-			return false;
-		if (typeCode == null) {
-			if (other.typeCode != null)
-				return false;
-		} else if (!typeCode.equals(other.typeCode))
-			return false;
-		return true;
-	}
 	
-
 }
