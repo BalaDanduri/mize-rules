721ef7d2e15087abb06fc5e1b09e5f6e18186692
harishburra <harishburra@m-ize.com>
2013-11-19 15:32:09 +0530

    Changes related to new Form domains

--- src/test/java/com/mize/domain/test/util/JPATest.java
+++ src/test/java/com/mize/domain/test/util/JPATest.java
@@ -4,6 +4,7 @@ package com.mize.domain.test.util;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 
+import org.h2.tools.Console;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.runner.RunWith;
@@ -15,6 +16,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import com.mize.domain.catalog.Catalog;
 import com.mize.domain.catalog.CatalogEntry;
 import com.mize.domain.common.Locale;
+import com.mize.domain.form.FormDefinition;
+import com.mize.domain.form.FormTemplateDefinition;
 import com.mize.domain.servicelocator.BusinessEntity;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -38,7 +41,7 @@ public class JPATest {
 	@BeforeClass
 	public static void launchH2Console() throws Exception {
 		// uncomment it if want to see H2console
-		//Console.main(new String[]{});
+		Console.main(new String[]{});
 	}
 	
 	public <T> Object find(Class<T> name, Object pkey) {
@@ -89,6 +92,16 @@ public class JPATest {
 	public BusinessEntity findExistingBE(EntityManager entityManager) {
 		BusinessEntity be = (BusinessEntity) entityManager.find(BusinessEntity.class, new Long(961));
 		return be;
+	}
+	
+	public FormTemplateDefinition findExistingFormTemplateDef(EntityManager entityManager) {
+		FormTemplateDefinition layoutdef = (FormTemplateDefinition) entityManager.find(FormTemplateDefinition.class, new Long(1));
+		return layoutdef;
+	}
+	
+	public FormDefinition findExistingFormDefinition(EntityManager entityManager) {
+		FormDefinition formDef = (FormDefinition) entityManager.find(FormDefinition.class, new Long(1));
+		return formDef;
 	}
 
 }
