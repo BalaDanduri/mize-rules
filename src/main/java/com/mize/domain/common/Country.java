234d4873e7d6d95782950db43d0c4f02cfc7f1ef
raghavendraserikar <raghavendraserikar@m-ize.com>
2014-01-21 15:50:40 +0530

    updated domain

--- src/main/java/com/mize/domain/common/Country.java
+++ src/main/java/com/mize/domain/common/Country.java
@@ -3,21 +3,19 @@ package com.mize.domain.common;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 import org.codehaus.jackson.annotate.JsonBackReference;
 import org.codehaus.jackson.map.annotate.JsonSerialize;
 import org.codehaus.jackson.map.annotate.JsonSerialize.Inclusion;
-import org.hibernate.annotations.Fetch;
-import org.hibernate.annotations.FetchMode;
-import org.hibernate.annotations.GenericGenerator;
 
 import com.mize.domain.util.JPASerializer;
 
@@ -31,11 +29,12 @@ public class Country extends MizeEntity implements Comparable<Country>{
 	private String name;
 	private String code3;
 	private List<State> states = new ArrayList<State>();
+	private List<State> stateList;
 	
 	@Id
-	@GenericGenerator(name="countryId" , strategy="increment")
-	@GeneratedValue(generator="countryId")
-	@Column(name="country_id",nullable=false)
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "country_id", nullable = false, unique = true)
+	@Override
 	public Long getId() {
 		return id;
 	}
@@ -62,6 +61,14 @@ public class Country extends MizeEntity implements Comparable<Country>{
 		this.name = name;
 	}
 	
+	@Transient
+	public List<State> getStateList() {
+		return stateList;
+	}
+
+	public void setStateList(List<State> stateList) {
+		this.stateList = stateList;
+	}
 
 	@Override
 	public int hashCode() {
@@ -117,8 +124,7 @@ public class Country extends MizeEntity implements Comparable<Country>{
 		return EQUAL;		
 	}
 
-	@OneToMany(cascade={CascadeType.ALL}, fetch= FetchType.LAZY , mappedBy ="country")
-	@Fetch(value = FetchMode.SELECT)
+	@OneToMany(fetch= FetchType.LAZY , mappedBy ="country")
 	@JsonSerialize(using=JPASerializer.class,include=Inclusion.NON_NULL)
 	@JsonBackReference(value="country_states")
 	public List<State> getStates() {
