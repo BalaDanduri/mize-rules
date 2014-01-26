1a3175dd1567b5568d103122eb624c60230f3b83
harishburra <harishburra@m-ize.com>
2014-01-26 16:35:27 +0530

    Added message for Product catalog

--- src/main/java/com/mize/domain/util/MessageConstants.java
+++ src/main/java/com/mize/domain/util/MessageConstants.java
@@ -128,6 +128,7 @@ public abstract class MessageConstants implements Serializable{
 	public static final String ATLEAST_ONE_PRODUCT_NAME_REQUIRED = "ATLEAST_ONE_PRODUCT_NAME_DETAILS_REQUIRED";
 	public static final String PRODUCT_NAME_REQUIRED = "PRODUCT_NAME_REQUIRED";
 	public static final String DUPLICATE_BRAND_AND_MODEL = "DUPLICATE_BRAND_AND_MODEL";
+	public static final String PRODUCT_EXISTS_FOR_BRAND_MODEL = "PRODUCT_EXISTS_FOR_BRAND_MODEL";
 	public static final String DUPLICATE_PRODUCT_NAME = "DUPLICATE_PRODUCT_NAME";
 	public static final String DUPLICATE_CATEGORY = "DUPLICATE_CATEGORY";
 	public static final String DUPLICATE_INSTANCE_LINK = "DUPLICATE_INSTANCE_LINK";
