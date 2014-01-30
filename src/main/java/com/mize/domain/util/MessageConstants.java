0f72784b1c75c3f6e08dc3d5de7d630e9599d1d2
harishburra <harishburra@m-ize.com>
2014-01-31 00:18:47 +0530

    Added messages and change for upload messages

--- src/main/java/com/mize/domain/util/MessageConstants.java
+++ src/main/java/com/mize/domain/util/MessageConstants.java
@@ -144,6 +144,8 @@ public abstract class MessageConstants implements Serializable{
 	public static final String MODEL_REQUIRED = "MODEL_REQUIRED";
 	public static final String FORM_NAME_REQUIRED = "FORM_NAME_REQUIRED";
 	public static final String DUPLICATE_FORM_NAME = "DUPLICATE_FORM_NAME";
+	public static final String LOCALE_IS_REQUIRED = "LOCALE_IS_REQUIRED";
+	public static final String BUILD_DATE_LESS_THAN_SHIP_DATE = "BUILD_DATE_LESS_THAN_SHIP_DATE";
 }
 
 
