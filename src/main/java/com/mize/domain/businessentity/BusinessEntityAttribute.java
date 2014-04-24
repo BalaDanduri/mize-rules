b4f0633af32d7acc9421cd55b1b4bafbe5697f62
HarishBurra <harishburra@m-ize.com>
2014-04-24 16:07:31 +0530

    Changed Tenant to lazy

--- src/main/java/com/mize/domain/businessentity/BusinessEntityAttribute.java
+++ src/main/java/com/mize/domain/businessentity/BusinessEntityAttribute.java
@@ -7,7 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
@@ -40,7 +40,7 @@ public class BusinessEntityAttribute extends MizeEntity implements Comparable<Bu
 		this.id = id;
 	}
 	
-	@ManyToOne(fetch=FetchType.LAZY)
+	@OneToOne(fetch=FetchType.LAZY)
 	@JoinColumn(name="be_id")
 	@JsonBackReference(value="be_attribute")
 	public BusinessEntity getBusinessEntity() {
