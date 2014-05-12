2e00963a1f9f50b1e7263ae4c17ecb930a34bf1f
Amarendra Tata <amarendratata@m-ize.com>
2014-05-12 13:59:30 +0530

    Added Brand and category to Formdefinition link.

--- src/main/java/com/mize/domain/form/link/FormDefinitionLinkData.java
+++ src/main/java/com/mize/domain/form/link/FormDefinitionLinkData.java
@@ -13,8 +13,10 @@ import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.mize.domain.brand.Brand;
 import com.mize.domain.common.MizeEntity;
 import com.mize.domain.product.Product;
+import com.mize.domain.product.ProductCategory;
 
 @Entity
 @Table(name = "form_defn_link_data", uniqueConstraints = {@UniqueConstraint (columnNames = {"id"})})
@@ -24,6 +26,8 @@ public class FormDefinitionLinkData extends MizeEntity {
 	
 	private FormDefinitionLink formDefinitionLink;
 	private Product product;
+	private Brand brand;
+	private ProductCategory category;
 	private String linkType;
 	private String linkDuration;
 	
@@ -135,6 +139,26 @@ public class FormDefinitionLinkData extends MizeEntity {
 	public String toString() {
 		return "FormDefinitionLinkData [product=" + product + ", linkType="
 				+ linkType + ", linkDuration=" + linkDuration + "]";
+	}
+	
+	@OneToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="brand_id", nullable = true)
+	public Brand getBrand() {
+		return brand;
+	}
+
+	public void setBrand(Brand brand) {
+		this.brand = brand;
+	}
+	
+	@OneToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="prod_cat_id", nullable = true)
+	public ProductCategory getCategory() {
+		return category;
+	}
+
+	public void setCategory(ProductCategory category) {
+		this.category = category;
 	}	
 	
 }
