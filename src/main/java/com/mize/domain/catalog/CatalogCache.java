d0057658a409667be30711df6276144a2bec9be6
suryamanem <suryamanem@m-ize.com>
2014-05-20 10:42:44 +0530

    Updated Domains

--- src/main/java/com/mize/domain/catalog/CatalogCache.java
+++ src/main/java/com/mize/domain/catalog/CatalogCache.java
@@ -7,33 +7,21 @@ import java.util.List;
 import org.joda.time.DateTime;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.mize.domain.businessentity.BusinessEntity;
-import com.mize.domain.common.MizeEntity;
 import com.mize.domain.util.JodaDateTimeDeserializer;
 import com.mize.domain.util.JsonDateTimeSerializer;
 
-public class CatalogCache extends MizeEntity implements Serializable{
+public class CatalogCache implements Serializable{
 	
 	private static final long serialVersionUID = -8488237770262609141L;	
 	private String catalogName;
 	private Long catalogId;	
 	private String isActive;
 	private List<CatalogEntryCache> catalogEntryCaches = new ArrayList<CatalogEntryCache>();
-	private List<Catalog> catalogNames = new ArrayList<Catalog>();	
 	private Long tenantId;
-	private BusinessEntity tenant;
-	
-	@Override
-	public Long getId() {
-		return catalogId;
-	}
-	@Override
-	public void setId(Long id) {
-		this.id= catalogId;
-	}
+	@DateTimeFormat (pattern="MM-dd-yyyy h:mm:ss")
+	private DateTime updatedDate;	
 	
 	public Long getCatalogId() {
 		return catalogId;
@@ -62,34 +50,18 @@ public class CatalogCache extends MizeEntity implements Serializable{
 		this.isActive = isActive;
 	}
 	
-	@Override	
+	
 	@DateTimeFormat(pattern = "MM-dd-yyyy h:mm:ss")
 	@JsonSerialize(using = JsonDateTimeSerializer.class, include = JsonSerialize.Inclusion.NON_DEFAULT)
-	@JsonIgnore(false)
 	public DateTime getUpdatedDate() {
 		return updatedDate;
 	}
 	
-	@Override	
+
 	@DateTimeFormat(pattern = "MM-dd-yyyy h:mm:ss")
 	@JsonDeserialize(using = JodaDateTimeDeserializer.class)
-	@JsonIgnore(false)
 	public void setUpdatedDate(DateTime updatedDate) {
 		this.updatedDate = updatedDate;
-	}
-	
-	public List<Catalog> getCatalogNames() {
-		return catalogNames;
-	}
-	public void setCatalogNames(List<Catalog> catalogNames) {
-		this.catalogNames = catalogNames;
-	}
-
-	public BusinessEntity getTenant() {
-		return tenant;
-	}
-	public void setTenant(BusinessEntity tenant) {
-		this.tenant = tenant;
 	}
 	
 	public Long getTenantId() {
@@ -98,15 +70,14 @@ public class CatalogCache extends MizeEntity implements Serializable{
 	public void setTenantId(Long tenantId) {
 		this.tenantId = tenantId;
 	}
-	
 	@Override
 	public String toString() {
 		return "CatalogCache [catalogName=" + catalogName + ", catalogId="
 				+ catalogId + ", isActive=" + isActive
 				+ ", catalogEntryCaches=" + catalogEntryCaches
-				+ ", catalogNames=" + catalogNames + ", tenantId=" + tenantId
-				+ ", tenant=" + tenant + "]";
-	}	
+				+ ", tenantId=" + tenantId
+				+ "]";
+	}
 }
 	
 
--- src/main/java/com/mize/domain/catalog/CatalogEntity.java
+++ src/main/java/com/mize/domain/catalog/CatalogEntity.java
@@ -4,25 +4,30 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.joda.time.DateTime;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.mize.domain.auth.User;
 import com.mize.domain.businessentity.BusinessEntity;
 import com.mize.domain.common.MizeEntity;
+import com.mize.domain.util.JodaDateTimeDeserializer;
+import com.mize.domain.util.JsonDateTimeSerializer;
 
 public class CatalogEntity extends MizeEntity implements Serializable{
 	
 	private static final long serialVersionUID = -8481137770262609141L;	
 
+	private String catalogName;
+	private Long catalogId;	
+	private String isActive;
 	private List<CatalogEntryCache> catalogEntryCaches = new ArrayList<CatalogEntryCache>();
 	private List<Catalog> catalogNames = new ArrayList<Catalog>();	
 	private BusinessEntity tenant;
 	private User user;
 	
-	public List<CatalogEntryCache> getCatalogEntryCaches() {
-		return catalogEntryCaches;
-	}
-	public void setCatalogEntryCaches(List<CatalogEntryCache> catalogEntryCaches) {
-		this.catalogEntryCaches = catalogEntryCaches;
-	}
 	@Override
 	public Long getId() {
 		return id;
@@ -31,6 +36,36 @@ public class CatalogEntity extends MizeEntity implements Serializable{
 	public void setId(Long id) {
 		this.id = id;
 	}
+	
+	public String getCatalogName() {
+		return catalogName;
+	}
+	public void setCatalogName(String catalogName) {
+		this.catalogName = catalogName;
+	}
+	
+	public Long getCatalogId() {
+		return catalogId;
+	}
+	public void setCatalogId(Long catalogId) {
+		this.catalogId = catalogId;
+	}
+	
+	public boolean isActive() {
+		return "Y".equalsIgnoreCase(isActive);
+	}
+
+	public void setIsActive(String isActive) {
+		this.isActive = isActive;
+	}
+	
+	public List<CatalogEntryCache> getCatalogEntryCaches() {
+		return catalogEntryCaches;
+	}
+	public void setCatalogEntryCaches(List<CatalogEntryCache> catalogEntryCaches) {
+		this.catalogEntryCaches = catalogEntryCaches;
+	}
+	
 	public List<Catalog> getCatalogNames() {
 		return catalogNames;
 	}
@@ -48,6 +83,22 @@ public class CatalogEntity extends MizeEntity implements Serializable{
 	}
 	public void setUser(User user) {
 		this.user = user;
+	}
+	
+	@Override	
+	@DateTimeFormat(pattern = "MM-dd-yyyy h:mm:ss")
+	@JsonSerialize(using = JsonDateTimeSerializer.class, include = JsonSerialize.Inclusion.NON_DEFAULT)
+	@JsonIgnore(false)
+	public DateTime getUpdatedDate() {
+		return updatedDate;
+	}
+	
+	@Override	
+	@DateTimeFormat(pattern = "MM-dd-yyyy h:mm:ss")
+	@JsonDeserialize(using = JodaDateTimeDeserializer.class)
+	@JsonIgnore(false)
+	public void setUpdatedDate(DateTime updatedDate) {
+		this.updatedDate = updatedDate;
 	}
 	
 }
