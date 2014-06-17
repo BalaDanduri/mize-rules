e34227310b4d2070725dd5d25bf8b348b5ff92ca
HemanthAppili <HemanthAppili@m-ize.com>
2014-06-17 17:41:33 +0530

    solr Definition changes

--- src/main/java/com/mize/domain/util/CatalogConstants.java
+++ src/main/java/com/mize/domain/util/CatalogConstants.java
@@ -61,6 +61,8 @@ public abstract class CatalogConstants implements Serializable{
 	public static final String SHIP_LOCATION_TYPE = "ShipLocationType";
 	public static final String LABOR_TYPE = "LaborType";
 	public static final String PAGE_SIZE = "PageSize";
+	public static final String COUNTRY = "country";
+	public static final String MANUFACTURER_TYPE = "ManufacturerType";
 
 }
 
