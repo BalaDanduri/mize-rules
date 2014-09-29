f4ee4037299789749066f5db4ef1d3468e4412d1
Ravi Kumar Ponduru <RaviPonduru@m-ize.com>
2014-09-29 20:20:31 +0530

    Form label changes.

--- src/main/java/com/mize/domain/form/Form.java
+++ src/main/java/com/mize/domain/form/Form.java
@@ -3,6 +3,9 @@ package com.mize.domain.form;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.mize.domain.applicationlabel.ApplicationLabel;
+
 public class Form extends FormElement{
 
 	/**
@@ -11,8 +14,19 @@ public class Form extends FormElement{
 	private static final long serialVersionUID = 7727863385524569981L;
 	private final List<SectionGroup> sectionGroups = new ArrayList<SectionGroup>();
 	
+	private List<ApplicationLabel> appLabels;
+	
 	public List<SectionGroup> getSectionGroups() {
 		return sectionGroups;
+	}
+
+	@JsonIgnore
+	public List<ApplicationLabel> getAppLabels() {
+		return appLabels;
+	}
+
+	public void setAppLabels(List<ApplicationLabel> appLabels) {
+		this.appLabels = appLabels;
 	}
 	
 }
