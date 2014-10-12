4c13e6f39aec0f885c1ee5cdeee88af73913e371
baladanduri <baladanduri@MIN-0414-139.mizeidc.mize.local>
2014-10-12 13:07:17 +0530

    poc changes

---
+++ src/main/java/com/mize/domain/entityparameter/EntityParameterNew.java
@@ -0,0 +1,18 @@
+package com.mize.domain.entityparameter;
+
+
+//@Entity
+//@Table(name = "entity_parameter", uniqueConstraints = { @UniqueConstraint(columnNames = {"id"}) })
+public class EntityParameterNew extends EntityParameter{
+
+	private static final long serialVersionUID = 841149014756400338L;
+	
+	
+	public EntityParameterNew() {
+	}
+	
+	//@Column(name = "entity_code",length = 50)
+	public String getCode() {
+		return super.getCode();
+	}
+}
