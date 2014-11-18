ba7a2ff438dc4051a2fc7f302aa44564b1d7b8f5
baladanduri <baladanduri@m-ize.com>
2014-11-18 13:28:09 +0530

    labor hour domain changes

--- src/main/java/com/mize/domain/labor/LaborHourExt.java
+++ src/main/java/com/mize/domain/labor/LaborHourExt.java
@@ -7,14 +7,10 @@ import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorColumn;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.mize.domain.businessentity.BusinessEntity;
 import com.mize.domain.util.MizeDateTime;
 
 @Entity
@@ -25,7 +21,6 @@ public class LaborHourExt extends LaborHour {
 	private static final long serialVersionUID = 81846948813348698L;
 	
 	private String isActive;
-	private BusinessEntity businessEntity;
 	
 	public LaborHourExt(){
 		super();
@@ -34,16 +29,6 @@ public class LaborHourExt extends LaborHour {
 	public LaborHourExt(Long id , String code, String type, MizeDateTime startDate,	MizeDateTime endDate, BigDecimal hours, String isActive) {		
 		super(id, code, type, startDate, endDate, hours);
 		this.isActive = isActive;
-	}
-	
-	@OneToOne(fetch=FetchType.EAGER)
-	@JoinColumn(name="labor_hour_be_id")
-	public BusinessEntity getBusinessEntity() {
-		return businessEntity;
-	}
-
-	public void setBusinessEntity(BusinessEntity businessEntity) {
-		this.businessEntity = businessEntity;
 	}
 	
 	@Column(name = "is_active")
