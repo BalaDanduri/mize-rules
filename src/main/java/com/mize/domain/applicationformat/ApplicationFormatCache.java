b4c0e90eb8ff8507f89f8b0d934069407eb0dc02
baladanduri <baladanduri@MIN-0414-139.mizeidc.mize.local>
2015-03-02 20:33:27 +0530

    updated domains

--- src/main/java/com/mize/domain/applicationformat/ApplicationFormatCache.java
+++ src/main/java/com/mize/domain/applicationformat/ApplicationFormatCache.java
@@ -12,6 +12,7 @@ public class ApplicationFormatCache implements Serializable, Comparable<Applicat
 	private String isActive;
 	private String formatTypeName;
 	private Long id;
+	private String isRegEXp;
 
 	public ApplicationFormatCache() {
 	}
@@ -80,16 +81,33 @@ public class ApplicationFormatCache implements Serializable, Comparable<Applicat
 		this.formatTypeName = formatTypeName;
 	}
 
+	public String getIsRegEXp() {
+		return isRegEXp;
+	}
+
+	public void setIsRegEXp(String isRegEXp) {
+		this.isRegEXp = isRegEXp;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
-		int result = super.hashCode();
+		int result = 1;
 		result = prime * result
 				+ ((formatType == null) ? 0 : formatType.hashCode());
 		result = prime * result
+				+ ((formatTypeName == null) ? 0 : formatTypeName.hashCode());
+		result = prime * result
 				+ ((formatValue == null) ? 0 : formatValue.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result
 				+ ((isActive == null) ? 0 : isActive.hashCode());
+		result = prime * result
+				+ ((isRegEXp == null) ? 0 : isRegEXp.hashCode());
+		result = prime * result
+				+ ((localeId == null) ? 0 : localeId.hashCode());
+		result = prime * result
+				+ ((tenantId == null) ? 0 : tenantId.hashCode());
 		return result;
 	}
 
@@ -97,7 +115,7 @@ public class ApplicationFormatCache implements Serializable, Comparable<Applicat
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
-		if (!super.equals(obj))
+		if (obj == null)
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
@@ -107,17 +125,51 @@ public class ApplicationFormatCache implements Serializable, Comparable<Applicat
 				return false;
 		} else if (!formatType.equals(other.formatType))
 			return false;
+		if (formatTypeName == null) {
+			if (other.formatTypeName != null)
+				return false;
+		} else if (!formatTypeName.equals(other.formatTypeName))
+			return false;
 		if (formatValue == null) {
 			if (other.formatValue != null)
 				return false;
 		} else if (!formatValue.equals(other.formatValue))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
 			return false;
 		if (isActive == null) {
 			if (other.isActive != null)
 				return false;
 		} else if (!isActive.equals(other.isActive))
 			return false;
+		if (isRegEXp == null) {
+			if (other.isRegEXp != null)
+				return false;
+		} else if (!isRegEXp.equals(other.isRegEXp))
+			return false;
+		if (localeId == null) {
+			if (other.localeId != null)
+				return false;
+		} else if (!localeId.equals(other.localeId))
+			return false;
+		if (tenantId == null) {
+			if (other.tenantId != null)
+				return false;
+		} else if (!tenantId.equals(other.tenantId))
+			return false;
 		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "ApplicationFormatCache [formatType=" + formatType
+				+ ", tenantId=" + tenantId + ", localeId=" + localeId
+				+ ", formatValue=" + formatValue + ", isActive=" + isActive
+				+ ", formatTypeName=" + formatTypeName + ", id=" + id
+				+ ", isRegEXp=" + isRegEXp + "]";
 	}
 
 	@Override
