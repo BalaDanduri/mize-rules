3e173c64dd429908cf1d822ba122dedf3b276b99
PrasannaNagaraju <PrasannaNagaraju@m-ize.com>
2015-04-01 12:43:35 +0530

    fixed test case  issue

--- src/test/java/com/mize/domain/appmsg/AppMessageTest.java
+++ src/test/java/com/mize/domain/appmsg/AppMessageTest.java
@@ -1,25 +1,13 @@
 package com.mize.domain.appmsg;
 
-import static org.junit.Assert.assertTrue;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 
-import org.joda.time.DateTimeZone;
 import org.junit.Before;
 import org.junit.Test;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.test.context.ContextConfiguration;
 
 import com.mize.domain.businessentity.BusinessEntity;
-import com.mize.domain.common.Locale;
 import com.mize.domain.test.util.JPATest;
-@ContextConfiguration(locations={"/test-context.xml"})
 public class AppMessageTest extends JPATest {
 
 	private static final String APPMESSAGE_QUERY = "select * from application_messages where id =?";
