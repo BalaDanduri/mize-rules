370646f19c78fd24bc54345664b52dda23e31851
sivagangabejawada <sivagangabejawada@m-ize.com>
2015-04-02 11:42:57 +0530

    Entity XRef criteria domain field change

--- src/main/java/com/mize/domain/xref/EntityXRefCriteria.java
+++ src/main/java/com/mize/domain/xref/EntityXRefCriteria.java
@@ -12,8 +12,8 @@ public class EntityXRefCriteria extends MizeSceEntity implements IEntity, Serial
 	private static final long serialVersionUID = -3193421149712191082L;
 
 	private String key;
-	private String code;
 	private String type;
+	private String value;
 
 	public EntityXRefCriteria() {
 		super();
@@ -28,20 +28,20 @@ public class EntityXRefCriteria extends MizeSceEntity implements IEntity, Serial
 		this.key = key;
 	}
 
-	public String getCode() {
-		return code;
-	}
-
-	public void setCode(String code) {
-		this.code = code;
-	}
-
 	public String getType() {
 		return type;
 	}
 
 	public void setType(String type) {
 		this.type = type;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
 	}
 
 	@JsonIgnore
@@ -109,14 +109,14 @@ public class EntityXRefCriteria extends MizeSceEntity implements IEntity, Serial
 
 	@Override
 	public String toString() {
-		return "EntityXRefCriteria [code=" + code + ", type=" + type + "]";
+		return "EntityXRefCriteria [value=" + value + ", type=" + type + "]";
 	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = super.hashCode();
-		result = prime * result + ((code == null) ? 0 : code.hashCode());
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
 		result = prime * result + ((type == null) ? 0 : type.hashCode());
 		return result;
 	}
@@ -130,10 +130,10 @@ public class EntityXRefCriteria extends MizeSceEntity implements IEntity, Serial
 		if (getClass() != obj.getClass())
 			return false;
 		EntityXRefCriteria other = (EntityXRefCriteria) obj;
-		if (code == null) {
-			if (other.code != null)
+		if (value == null) {
+			if (other.value != null)
 				return false;
-		} else if (!code.equals(other.code))
+		} else if (!value.equals(other.value))
 			return false;
 		if (type == null) {
 			if (other.type != null)
